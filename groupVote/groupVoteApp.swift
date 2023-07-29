5720e9ef11adb0aaf93c3d9de402e50ab76a4830
AlloTheOne <allo.busy@gmail.com>
2023-07-29 20:58:39 +0300

    Initial Commit

---
+++ groupVote/groupVoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  groupVoteApp.swift
+//  groupVote
+//
+//  Created by Alaa . on 29/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct groupVoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
